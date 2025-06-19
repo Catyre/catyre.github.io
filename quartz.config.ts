dcc254860530de21cad0a5c540bf7a1b9e779f68
Catyre <calebremo99@gmail.com>
2025-06-19 09:58:21 -0400

    Quartz sync: Jun 19, 2025, 9:58 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,6 +56,7 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
+      Plugin.HardLineBreaks(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
       }),
